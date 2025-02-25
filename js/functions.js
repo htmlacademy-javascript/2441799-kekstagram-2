8334750db7ffd769d6b33adff4889bf939f1ad4a
Ekaterina <kate.sitnikova@mail.ru>
2025-02-25 23:15:41 +0300

    вносит изменения в функцию палиндрома

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,7 @@ function checkLength1 (string, length) {
   } else {
     return false;
   }
-};
+}
 
 // Строка короче 20 символов
 console.log (checkLength1 ('проверяемая строка', 20));
@@ -18,11 +18,27 @@ console.log (checkLength1 ('проверяемая строка', 10));
 
 //Функции для проверки, является ли строка палиндромом
 function checkPalindrome (value) {
-  const string = value.replaceAll().toLowerCase ();
-  const str = '';
-  for (let i = 0; i < string.length; i--) {
-
+  const string = value.replaceAll('').toLowerCase();
+  let str = '';
+  for (let i = string.length - 1; i >= 0; i--) {
+    str += string[i];
+    if (str === string) {
+      return 'Строка является палиндромом';
+    } else {
+      return 'Строка не является палиндромом';
+    }
   }
-};
+}
 
-console.log (checkPalindrome('овод'));
+console.log (checkPalindrome('довод'));
+
+/*function checkPalindrome1 (value) {
+  const string = value.replaceAll('').toLowerCase();
+  if (string === value.split('').reverse().join('')) {
+    return 'Строка является палиндромом';
+  } else {
+    return 'Строка не является палиндромом';
+  }
+}
+
+console.log (checkPalindrome1('анна'));*/
