d5bd9e1a93c8e2171989df37c32a65c17f04fcf1
Ekaterina <kate.sitnikova@mail.ru>
2025-10-24 18:34:48 +0300

    добавляет обработчики клика

--- js/form.js
+++ js/form.js
@@ -3,12 +3,20 @@ const imgUploadForm = document.querySelector ('.img-upload__form');
 const imgUploadInput = imgUploadForm.querySelector ('.img-upload__input');
 const imgUploadOverlay = imgUploadForm.querySelector ('.img-upload__overlay');
 const body = document.querySelector ('body');
+const imgUploadCancel = imgUploadForm.querySelector ('.img-upload__cancel');
+
+const onModalEditorCancelClick = () => {
+  closeModalEditor();
+};
+
+function closeModalEditor () {
+  imgUploadCancel.removeEventListener ('click', onModalEditorCancelClick);
+}
 
 const openModalEditor = () => {
   imgUploadInput.addEventListener ('change', () => {
     imgUploadOverlay.classList.remove ('hidden');
     body.classList.add ('modal-open');
+    imgUploadCancel.addEventListener ('click', onModalEditorCancelClick);
   });
 };
-
-openModal();
