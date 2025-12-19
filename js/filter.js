0397191213c5be8db09cfdab55f1432c474c77aa
Ekaterina <kate.sitnikova@mail.ru>
2025-12-19 21:07:13 +0300

    добавляет функцию переключения активности кнопки

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,20 @@
-const filterElement = document.querySelector('.img-filters');
+const imgFiltersElement = document.querySelector('.img-filters');
+const imgFiltersForm = document.querySelector('.img-filters__form');
+
+//функция убирает отрисованные фотографии
+const clearThumbnail = () => {
+  document.querySelectorAll('.picture').forEach((element) => element.remove());
+};
+
+//функция переключает активность кнопки
+const setActiveButton = (button) => {
+  imgFiltersForm.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+  button.classList.add('img-filters__button--active');
+};
+
+
 
 //функция для показа блока фильтров
 export const showImgFilters = () => {
-  filterElement.classList.remove('img-filters--inactive');
+  imgFiltersElement.classList.remove('img-filters--inactive');
 };
