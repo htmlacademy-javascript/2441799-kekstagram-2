25b4e7326b8ada4c0fbe142ec6d1797d58aa7f44
Ekaterina <kate.sitnikova@mail.ru>
2025-12-19 22:07:19 +0300

    добавляет фильтры по умолчанию и случайные

--- js/filter.js
+++ js/filter.js
@@ -1,3 +1,6 @@
+import { renderCards } from "./photos/thumbnails";
+
+const RANDOM_PICTURES_COUNT = 10;
 const imgFiltersElement = document.querySelector('.img-filters');
 const imgFiltersForm = document.querySelector('.img-filters__form');
 
@@ -12,7 +15,23 @@ const setActiveButton = (button) => {
   button.classList.add('img-filters__button--active');
 };
 
+//фильтр по-умолчанию
+const applyDefaultFilter = () => {
+  clearThumbnail();
+  renderCards(pictures);
+};
 
+//фильтр случайные
+const applyRandomFilter = () => {
+  clearThumbnail();
+
+  const randomPictures = pictures
+  .slice()
+  .sort(() => Math.random() - 0.5)
+  .slice(0, RANDOM_PICTURES_COUNT);
+
+  renderCards(randomPictures);
+};
 
 //функция для показа блока фильтров
 export const showImgFilters = () => {
