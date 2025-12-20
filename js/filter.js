7b31f6f81b960b120d99afef604abc2016cd05cb
Ekaterina <kate.sitnikova@mail.ru>
2025-12-20 21:43:34 +0300

    исправлены ошибки в функции переключения активности кнопки, добавляет обработчик клика на фильтры

--- js/filter.js
+++ js/filter.js
@@ -1,9 +1,13 @@
-import { renderCards } from "./photos/thumbnails";
+import { renderCards } from "./photos/thumbnails.js";
 
 const RANDOM_PICTURES_COUNT = 10;
 const imgFiltersElement = document.querySelector('.img-filters');
 const imgFiltersForm = document.querySelector('.img-filters__form');
-let pictures = [];
+
+export let pictures = [];
+export const setPictures = (data) => {
+  pictures = data;
+};
 
 //функция убирает отрисованные фотографии
 const clearThumbnail = () => {
@@ -12,13 +16,19 @@ const clearThumbnail = () => {
 
 //функция переключает активность кнопки
 const setActiveButton = (button) => {
-  imgFiltersForm.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+  const currentActiveButton = imgFiltersForm.querySelector('.img-filters__button--active');
+
+  if (currentActiveButton) {
+    currentActiveButton.classList.remove('img-filters__button--active');
+  }
+
   button.classList.add('img-filters__button--active');
 };
 
 //фильтр по-умолчанию
 const applyDefaultFilter = () => {
   clearThumbnail();
+
   renderCards(pictures);
 };
 
@@ -40,13 +50,40 @@ const applyDiscussedFilter = () => {
 
   const sortedPictures = pictures
     .slice()
-    .sort((a, b) => b.commens.length - a.comments.length);
+    .sort((a, b) => b.comments.length - a.comments.length);
 
-    renderCards(sortedPictures);
+  renderCards(sortedPictures);
 };
 
+//обработчик клика на фильтры
+const onFilterClick = (evt) => {
+  if(!evt.target.classList.contains('img-filters__button')) {
+    return;
+  }
+
+  setActiveButton(evt.target);
+
+  switch (evt.target.id) {
+    case 'filter-default':
+    applyDefaultFilter();
+    break;
+
+    case 'filter-random':
+    applyRandomFilter();
+    break;
+
+    case 'filter-discussed':
+    applyDiscussedFilter();
+    break;
+  }
+};
 
 //функция для показа блока фильтров
 export const showImgFilters = () => {
+  if(!imgFiltersElement || !imgFiltersForm) {
+    return;
+  }
+
   imgFiltersElement.classList.remove('img-filters--inactive');
+  imgFiltersForm.addEventListener('click', onFilterClick)
 };
