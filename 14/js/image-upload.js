52980f2d800b5bf23ea70bd63ecbced308c2e8ea
Keks <keks@htmlacademy.ru>
2025-12-21 00:17:01 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/image-upload.js
@@ -0,0 +1,46 @@
+import { resetEffects } from "./effect.js";
+import { DEFAULT_VALUE, imgPreview, updateScale } from "./scale.js";
+
+export const initUpload = () => {
+  const uploadInput = document.querySelector('.img-upload__input');
+  const uploadPreview = document.querySelector('.img-upload__preview img');
+
+  const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
+  if (!uploadInput || !uploadPreview) return;
+
+  uploadInput.addEventListener('change', () => {
+    const file = uploadInput.files[0];
+
+    if (!file) return;
+
+    const fileName = file.name.toLowerCase();
+    const isValidType = FILE_TYPES.some((type) => fileName.endsWith(type));
+
+    if (!isValidType) {
+      alert('Выберите изображение в формате JPG, JPEG или PNG!');
+      uploadInput.value = ''; // сброс выбранного файла
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      const fileURL = reader.result;
+      imgPreview.src = fileURL;
+
+      //обновляем миниатюры эффектов
+      const effectsPreviews = document.querySelectorAll('.effects__preview');
+      effectsPreviews.forEach((preview) => {
+        preview.style.backgroundImage = `url(${fileURL})`;
+      });
+
+      //сброс масштаба и эффектов
+      updateScale(DEFAULT_VALUE);
+      resetEffects();
+    });
+
+    reader.readAsDataURL(file);
+  });
+
+};
