4ac6e689dd6253e07e58b0740a07846a58f092b1
Keks <keks@htmlacademy.ru>
2025-12-23 16:25:08 +0300

    :heavy_check_mark: Сборка #15

---
+++ 15/js/filter.js
@@ -0,0 +1,91 @@
+import { renderCards } from "./photos/thumbnails.js";
+import { debounce } from "./util.js";
+
+const RANDOM_PICTURES_COUNT = 10;
+const imgFiltersElement = document.querySelector('.img-filters');
+const imgFiltersForm = document.querySelector('.img-filters__form');
+
+export let pictures = [];
+export const setPictures = (data) => {
+  pictures = data;
+};
+
+//функция убирает отрисованные фотографии
+const clearThumbnail = () => {
+  document.querySelectorAll('.picture').forEach((element) => element.remove());
+};
+
+//функция переключает активность кнопки
+const setActiveButton = (button) => {
+  const currentActiveButton = imgFiltersForm.querySelector('.img-filters__button--active');
+
+  if (currentActiveButton) {
+    currentActiveButton.classList.remove('img-filters__button--active');
+  }
+
+  button.classList.add('img-filters__button--active');
+};
+
+//фильтр по-умолчанию
+const applyDefaultFilter = () => {
+  clearThumbnail();
+
+  renderCards(pictures);
+};
+
+//фильтр случайные
+const applyRandomFilter = () => {
+  clearThumbnail();
+
+  const randomPictures = pictures
+  .slice()
+  .sort(() => 0.5 - Math.random())
+  .slice(0, RANDOM_PICTURES_COUNT);
+
+  renderCards(randomPictures);
+};
+
+//фильтр обсуждаемые
+const applyDiscussedFilter = () => {
+  clearThumbnail();
+
+  const sortedPictures = pictures
+    .slice()
+    .sort((a, b) => b.comments.length - a.comments.length);
+
+  renderCards(sortedPictures);
+};
+
+//обработчик клика на фильтры
+const onFilterClick = (evt) => {
+  if(!evt.target.classList.contains('img-filters__button')) {
+    return;
+  }
+
+  switch (evt.target.id) {
+    case 'filter-default':
+    applyDefaultFilter();
+    break;
+
+    case 'filter-random':
+    applyRandomFilter();
+    break;
+
+    case 'filter-discussed':
+    applyDiscussedFilter();
+    break;
+  }
+};
+
+const onFilterClickDebounced = debounce(onFilterClick);
+
+//функция для показа блока фильтров
+export const showImgFilters = () => {
+  if(!imgFiltersElement || !imgFiltersForm) return;
+
+  imgFiltersElement.classList.remove('img-filters--inactive');
+  imgFiltersForm.addEventListener('click', (evt) => {
+    setActiveButton(evt.target);
+    onFilterClickDebounced(evt);
+  });
+};
---
+++ 15/js/form.js
@@ -0,0 +1,130 @@
+import {isValid, resetValidation} from './validation.js';
+import { resetEffects } from './effect.js';
+import { resetScale } from './scale.js';
+import { sendData } from './api.js';
+
+const imgUploadForm = document.querySelector ('.img-upload__form');
+const imgUploadInput = imgUploadForm.querySelector ('.img-upload__input');
+const imgUploadOverlay = imgUploadForm.querySelector ('.img-upload__overlay');
+const body = document.querySelector ('body');
+const imgUploadCancel = imgUploadForm.querySelector ('.img-upload__cancel');
+const textHashtags = imgUploadForm.querySelector ('.text__hashtags');
+const textComment = imgUploadForm.querySelector ('.text__description');
+const successMessage = document.querySelector ('#success').content.querySelector('.success');
+const errorMessage = document.querySelector ('#error').content.querySelector('.error');
+
+function resetEditor() {
+  imgUploadForm.reset();
+  resetScale();
+  resetEffects();
+  resetValidation();
+};
+
+const onClickOutsideEditor = (evt) => {
+  if (evt.target === imgUploadOverlay) {
+    closeModalEditor ();
+  }
+};
+
+const onModalEditorCancelClick = () => {
+  closeModalEditor();
+};
+
+const onEscKeydown = (evt) => {
+  if (evt.key === 'Escape' && !document.querySelector('.error')) {
+    closeModalEditor();
+  }
+};
+
+function closeModalEditor () {
+  imgUploadOverlay.classList.add ('hidden');
+  body.classList.remove ('modal-open');
+  resetEditor();
+  document.removeEventListener('keydown', onEscKeydown);
+  imgUploadCancel.removeEventListener ('click', onModalEditorCancelClick);
+  imgUploadOverlay.removeEventListener('click', onClickOutsideEditor);
+  imgUploadInput.value = '';
+}
+
+//обработчик для кнопки сбоса
+imgUploadForm.addEventListener ('reset', () => {
+  resetEditor();
+});
+
+const openModalEditor = () => {
+  imgUploadInput.addEventListener ('change', () => {
+    imgUploadOverlay.classList.remove ('hidden');
+    body.classList.add ('modal-open');
+    document.addEventListener('keydown', onEscKeydown);
+    imgUploadCancel.addEventListener ('click', onModalEditorCancelClick);
+    imgUploadOverlay.addEventListener('click', onClickOutsideEditor);
+  });
+};
+
+openModalEditor ();
+
+// если фокус находится в поле ввода хэштега, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
+textHashtags.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    evt.stopPropagation();
+  }
+});
+
+//если фокус находится в поле ввода комментария, нажатие на Esc не должно приводить к закрытию формы редактирования изображения.
+textComment.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape' || evt.key === 'Esc') {
+    evt.stopPropagation();
+  }
+});
+
+//функция для сообщения об ошибках
+function showMessageLoading(template) {
+  const message = template.cloneNode(true); //клонирование шаблона сообщения
+  body.appendChild(message); //добавить на страницу перед </body>
+
+  const onEscKeydownMessage = (evt) => {
+    if (evt.key === 'Escape') {
+      closeMessage();
+    }
+  };
+
+  const onClickOutsideMessage = (evt) => {
+    if (evt.target.classList.contains('error') || evt.target.classList.contains('success')) {
+      closeMessage ();
+    }
+  };
+
+  const button = message.querySelector('.success__button') || message.querySelector('.error__button');
+  const closeMessage = () => {
+    message.remove ();
+    document.removeEventListener('keydown', onEscKeydownMessage);
+    document.removeEventListener('click', onClickOutsideMessage);
+  };
+
+  document.addEventListener('keydown', onEscKeydownMessage);
+  document.addEventListener('click', onClickOutsideMessage);
+
+  if (button) {
+    button.addEventListener('click', closeMessage);
+  }
+}
+
+//функция отправки формы
+imgUploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  if (!isValid()) {
+    return;
+  }
+
+  const imgUploadFormData = new FormData(imgUploadForm);
+
+  sendData(imgUploadFormData)
+   .then(() => {
+    closeModalEditor();
+    showMessageLoading(successMessage);
+   })
+   .catch(() => {
+    showMessageLoading(errorMessage);
+   });
+});
